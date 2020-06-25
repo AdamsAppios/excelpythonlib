f8b9aa36308b9fac0aa4e75848f3a47421fa95c1
AdamsAppios <23veclipse23v@gmail.com>
2020-06-25 15:00:23 +0800

    Added more functions

--- pandasfunctions/pandastringmanip.py
+++ pandasfunctions/pandastringmanip.py
@@ -1,8 +1,18 @@
 
 import pandas as pd
 
-# replaces unwanted left and rightparts of a string
+
+def replaceLeftRight(dfcol, left, right):
+    return dfcol.map(lambda x: x.lstrip(left).rstrip(right))
 
 
-def replaceLeftRight(df, left, right):
-    return df.map(lambda x: x.lstrip(left).rstrip(right))
+def expandColumnFromString(dfcol, strpat):
+    return dfcol.str.split(strpat, expand=True)
+
+
+# use this date_parser function to convert date string to datetime values eg '%Y-%m-%d %I-%p'
+pattern = '%Y-%m-%d %I-%p'
+
+
+def d_parser(x):
+    return pd.datetime.strptime(x, pattern)
--- xl.py
+++ xl.py
@@ -4,7 +4,20 @@ import pandas as pd
 from xlwingsfunctions.basicfunctions import *
 from pandasfunctions.pandastringmanip import *
 import numpy as np
-sht = xw.books['sample.xlsx'].sheets["Sheet1"]
-df = expandTable(sht, "A1", pd.DataFrame)
-df['result'] = replaceLeftRight(df['result'], "+-", "aAbBcC")
-print(df)
+dummy_data1 = {
+    'id': ['1', '2', '3', '4', '5'],
+    'Feature1': ['A', 'C', 'E', 'G', 'I'],
+    'Feature2': ['B', 'D', 'F', 'H', 'J']}
+df1 = pd.DataFrame(dummy_data1, columns=['id', 'Feature1', 'Feature2'])
+dummy_data2 = {
+    'id': ['1', '2', '6', '7', '8'],
+    'Feature1': ['K', 'M', 'O', 'Q', 'S'],
+    'Feature2': ['L', 'N', 'P', 'R', 'T']}
+df2 = pd.DataFrame(dummy_data2, columns=['id', 'Feature1', 'Feature2'])
+dummy_data3 = {
+    'id': ['1', '2', '3', '4', '5', '7', '8', '9', '10', '11'],
+    'Feature3': [12, 13, 14, 15, 16, 17, 15, 12, 13, 23]}
+df3 = pd.DataFrame(dummy_data3, columns=['id', 'Feature3'])
+frames = [df1, df2]
+df_keys = pd.concat(frames, keys=['x', 'y'])
+print(df_keys)
